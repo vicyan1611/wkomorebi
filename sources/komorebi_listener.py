331d0aef11f4d238d70e6d04b656881193912efa
Vinh <vinhpham123.np@gmail.com>
2024-05-17 22:12:54 +0700

    komorebi_listener: update username and komorebi_listener

--- sources/komorebi_listener.py
+++ sources/komorebi_listener.py
@@ -1,5 +1,8 @@
-import win32pipe
+import win32pipe, win32file
 import logging
+import time
+import json
+
 from komorebic_client import WKomorebic
 BUFFER_SIZE = 1024 * 64
 class KomorebiListener:
@@ -27,7 +30,29 @@ class KomorebiListener:
         self.komorebic.subscribe_pipe(self.pipename)
         print("connect successfully")
 
+    def read_komorebi_events(self) -> None:
+        try:
+            while True:
+                buffer, bytes_to_read, status = win32pipe.PeekNamedPipe(self.pipe, 1);
+                if not bytes_to_read:
+                    time.sleep(0.1)
+                    continue
+                
+                result, data = win32file.ReadFile(self.pipe, bytes_to_read)
+                if not data.strip():
+                    continue
+                
+                event = json.loads(data.decode("utf-8"))
+                event_name = event['event']['type']
+                event_state = event['event']['state']
+                if event_name and event_state:
+                    print(event_name)   
+                # break
+        except (BaseException, Exception):
+            win32file.CloseHandle(self.pipe)
+            # print(Exception)
 if __name__ == "__main__":
     tko = KomorebiListener()
     tko.create_named_pipe()
     tko.connect_komorebi()
+    tko.read_komorebi_events()
